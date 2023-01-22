a558a6ad629c5474fe6a7ac741a386736f8b8926
christ08g <123308233+christ08g@users.noreply.github.com>
2023-01-22 22:04:36 +0700

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -4,10 +4,10 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `PGPASSWORD=GjpS3pHHJ3YmiESWvShg psql -h containers-us-west-41.railway.app -U postgres -p 5898 -d railway`;
+const connectionString = `postgresql://postgres:GjpS3pHHJ3YmiESWvShg@containers-us-west-41.railway.app:5898/railway`;
 
 const pool = new Pool({
-  connectionString: `PGPASSWORD=GjpS3pHHJ3YmiESWvShg psql -h containers-us-west-41.railway.app -U postgres -p 5898 -d railway`
+  connectionString: `postgresql://postgres:GjpS3pHHJ3YmiESWvShg@containers-us-west-41.railway.app:5898/railway`
 });
 
 module.exports = { pool };
